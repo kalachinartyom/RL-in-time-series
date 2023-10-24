c524f00b32f774a4abe7a27cf4ed2b6f83e2c7bc
kalachtema <kalachtema@gmail.com>
2023-10-24 16:39:12 +0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Application of RL methods for time series forecasting',
+    author='Artyom Kalachin',
+    license='MIT',
+)
